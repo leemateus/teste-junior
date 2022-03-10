446262937829b78e64bbd022a07aca28b8ae214a
leemateus <leemateus2009@hotmail.com>
2022-03-09 23:38:46 -0300

    migrate cliente criada

---
+++ database/migrations/2022_03_10_014404_create_pedidos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePedidosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pedidos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->double('total', 6, 2);
+            $table->string('status_pedido', 10);
+            $table->usignedInteger('cliente_id');
+            $table->foreign('cliente_id')->references('id')->on('clientes')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pedidos');
+    }
+}
