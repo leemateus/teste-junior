47e7b5d91e593656d83779a8fa9354a508e99d5b
leemateus <leemateus2009@hotmail.com>
2022-03-09 22:27:56 -0300

    migration produto criada

---
+++ database/migrations/2022_03_10_011245_create_produtos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 50);
+            $table->string('quant_prod', 50);
+            $table->string('cod_barra', 150);
+            $table->double('valor_unitario', 5, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+}
