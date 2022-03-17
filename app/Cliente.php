d88a3d6a902b6a9efec37d3a81000177ce78791f
leemateus <leemateus2009@hotmail.com>
2022-03-16 22:23:25 -0300

    desevolvido model do cliente

--- app/Cliente.php
+++ app/Cliente.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    protected $fillable = ['nome', 'CPF', 'email'];
+
+    protected $hiden = ['id'];
+
+    public function pedidos()
+    {
+      return $this->hasMany('App\Pedido');
+    }
 }
